01c81420cc6046164b67a305f8fdb13ef5b49b6f
Hammad Mazhar <hammad.maz@gmail.com>
2014-03-08 10:52:24 -0600

    Simplify code Get rid of vector  and use aligned memory instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,19 @@
 #include "omp.h"
-#include <iostream>
-#include <vector>
+#include <stdio.h>
 #include <math.h>
-//#include <nmmintrin.h>
+#include <nmmintrin.h>
 using namespace std;
-#include "float4.h"
-
-// class __attribute__ ((aligned(16))) float4 {
-// public:
-// 	union {
-// 		struct {float x, y, z, w;};
-// 		__m128 mmvalue;
-// 	};
-// 	inline float4() 		:mmvalue(_mm_setzero_ps()) { }
-// 	inline float4(float a) 	:mmvalue(_mm_set1_ps(a)) {}
-// 	inline float4(__m128 m) :mmvalue(m) {}
-// 	inline float4 operator+(const float4& b) const {	return _mm_add_ps(mmvalue, b.mmvalue);}
-// };
+class __attribute__ ((aligned(16))) float4 {
+public:
+	union {
+		struct {float x, y, z, w;};
+		__m128 mmvalue;
+	};
+	inline float4() 		:mmvalue(_mm_setzero_ps()) { }
+	inline float4(float a) 	:mmvalue(_mm_set1_ps(a)) {}
+	inline float4(__m128 m) :mmvalue(m) {}
+	inline float4 operator+(const float4& b) const {	return _mm_add_ps(mmvalue, b.mmvalue);}
+};
 
 int main(int argc, char *argv[]) {
 	int thread_num = 1;
@@ -24,20 +21,18 @@ int main(int argc, char *argv[]) {
 	omp_set_num_threads(thread_num);
 
 	double runs = 27;
-	vector<float4> A (pow(2,runs));
-	vector<float4> B (pow(2,runs));
-	vector<float4> C (pow(2,runs));
-	vector<float4> D (pow(2,runs));
+	float4* A = (float4*) _mm_malloc (pow(2,runs)*sizeof(float4), 16 );
+	float4* B = (float4*) _mm_malloc (pow(2,runs)*sizeof(float4), 16 );
+	float4* C = (float4*) _mm_malloc (pow(2,runs)*sizeof(float4), 16 );
+	float4* D = (float4*) _mm_malloc (pow(2,runs)*sizeof(float4), 16 );
 	//Generate data
-	// #pragma omp parallel for 
-// 	for (int i = 0; i < 134217728; i++) {
-// 		A[i] = float4(1.0*i);
-// 		B[i] = float4(1.3/float(i));
-// 		//C[i] = float4(1.3/float(i));
-// 		//D[i] = float4(1.5/float(i));
-// 	}
-
-
+	#pragma omp parallel for 
+	for (int i = 0; i < 134217728; i++) {
+		A[i] = float4(i);
+		B[i] = float4(1.0/float(i));
+		C[i] = float4(2.0/float(i));
+		D[i] = float4(3.0/float(i));
+	}
 	for (int i = 14; i < runs; i++) {
 		unsigned int MAX_ITEMS  = pow(2,i);
 		//Clear the cache!
@@ -57,9 +52,9 @@ int main(int argc, char *argv[]) {
 		double end = omp_get_wtime();
 		printf("Execution time =  %0.3f ms | Memory Transfered:  %0.3f| Bandwidth  %0.3f GB/s \n", (end - start) * 1000, sizeof(float4)*MAX_ITEMS/1024.0/1024.0, (3 * 4 * 4) * MAX_ITEMS / ((end - start)) / 1024.0 / 1024.0 / 1024.0);
 	}
-	A.clear();
-	B.clear();
-	C.clear();
-	D.clear();
+	_mm_free(A);
+	_mm_free(B);
+	_mm_free(C);
+	_mm_free(D);
 	return 0;
 }
